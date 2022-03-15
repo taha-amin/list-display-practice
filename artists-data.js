f7ff355760fcdb533642c4fda3944dc8ca3720bf
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-15 16:28:05 -0700

    renamed and created artists-data.js file that contains data for artists and number of albums and their popular songs.

---
+++ artists-data.js
@@ -0,0 +1,22 @@
+export const artists = [
+    {
+        artist: 'Daft Punk',
+        albums: 4,
+        songs: ['Get Lucky', 'One More Time', 'Around the World']
+    },
+    {
+        artist: 'OutKast',
+        albums: 6,
+        songs: ['Ms. Jackson', 'Hey Ya!', 'So Fresh, So Clean']
+    },
+    {
+        artist: 'Modest Mouse',
+        albums: 7,
+        songs: ['Float On', 'Dashboard', 'Missed the Boat']
+    },
+    {
+        artist: '2pac',
+        albums: 4,
+        songs: ['California Love', 'Changes', 'Dear Mama', '2 of Amerikaz Most Wanted']
+    }
+];
